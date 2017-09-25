8c7f44049e4b404a5646b9a56a9d8348dabee219
AskAlice <AskAlice@users.noreply.github.com>
2017-09-24 18:06:26 -0600

    xd

--- 09-2017/assignment-4/Assignment4.cpp
+++ 09-2017/assignment-4/Assignment4.cpp
@@ -2,25 +2,61 @@
 // Recitation: CSCI 1300-210 X. Zhang
 //
 // Assignment 4
-// main.cpp
+// Assignment4.cpp
+
 #include <iostream>
 using namespace std;
 
-int distance(string const& str1, string const& str2){
-	if(str1.length() != str2.length()){
-		cout << "strings are different lengths, can't calculate distance" << endl;
-		return 0;
-	}
-	int diff = 0;
-	int i = 0;
-	while(i < str1.length()){
-		if(!(str1[i] == str2[i]))
-			diff += 1;
-        i++;
+int dist(string str1, string str2){
+    if(str1.length() != str2.length()){
+        cout << "Length Mismatch with " << str1 << " and " << str2 <<  "(Lengths of " << str1.length() << " and " << str2.length() << ")" << endl;
+        return 0;
     }
-    return diff;
+    int diff = 0;
+    for(int i=0; i < str1.length(); i++){
+        if(str1[i] != str2[i])
+            diff++;
+    }
+	return diff;
 }
 
-int main(){
-cout << distance("ATTC","ATTG");
+float similarityScore(string str1, string str2){
+    if(str1.length() != str2.length()){
+        return 0;
+    }
+    return (float) (str1.length() - dist(str1,str2)) / str1.length();
+}
+
+int countMatches(string genome, string sequence1, float min_score){
+    string matching = "";
+    int ct = 0;
+    for(int i=0; i<genome.length();i++){
+        float score = similarityScore(genome.substr(i,sequence1.length()),sequence1);
+        if(score >= min_score)
+            ct++;
+    }
+    return ct;
+}
+
+float findBestMatch(string genome, string seq){
+    float bestScore = 0;
+    for(int i=0; i<genome.length();i++){
+        float score = similarityScore(genome.substr(i,seq.length()),seq);
+        if(score >= bestScore)
+            bestScore = score;
+    }
+    return bestScore;
+}
+
+int findBestGenome(string genome1, string genome2, string genome3, string seq){
+    float g1 = findBestMatch(genome1,seq);
+    float g2 = findBestMatch(genome2,seq);
+    float g3 = findBestMatch(genome3,seq);
+    if (g3 >= g2 && g3 >= g1)
+        return 3;
+    if(g2 >= g3 && g2 >= g1)
+        return 2;
+    if(g1 >= g3 && g1 >= g2)
+        return 1;
+return 0;
 }
