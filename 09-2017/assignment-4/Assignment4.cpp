e93ba724c99fc5117e050e1c9c2b4726d041fad8
AskAlice <AskAlice@users.noreply.github.com>
2017-09-24 19:46:32 -0600

    sdlkfjsdf

--- 09-2017/assignment-4/Assignment4.cpp
+++ 09-2017/assignment-4/Assignment4.cpp
@@ -7,6 +7,13 @@
 #include <iostream>
 using namespace std;
 
+
+/*
+* dist takes two parameters, str1, and str2.
+* the strings must be the same length or else it will return zero.
+* returns an int which is the hamming distance of the two strings.
+* it uses a for loop to iterate through the strings, and if the characters don't match for a certain index, the distance increases
+*/
 int dist(string str1, string str2){
     if(str1.length() != str2.length()){
         cout << "Length Mismatch with " << str1 << " and " << str2 <<  "(Lengths of " << str1.length() << " and " << str2.length() << ")" << endl;
@@ -20,6 +27,12 @@ int dist(string str1, string str2){
 	return diff;
 }
 
+/*
+* similarityScore takes two parameters, str1, and str2.
+* the strings must be the same length or else it will return zero.
+* it returns a float using this formula:
+* (stringLength - hammingDistance) / stringLength
+*/
 float similarityScore(string str1, string str2){
     if(str1.length() != str2.length()){
         return 0;
@@ -27,10 +40,15 @@ float similarityScore(string str1, string str2){
     return (float) (str1.length() - dist(str1,str2)) / str1.length();
 }
 
+/*
+* countMatches takes three parameters, a genome and a sequence to compare, strings, and a minimum score, a float.
+* it counts how many matches of a certain sequence are in a genome, using a threshold of similarity.
+* uses a for loop that iterates through the genome string and checks the similarity score with a substring of the genome at that position, which is the length of the sequence to compare.
+* returns the count of matches found.
+*/
 int countMatches(string genome, string sequence1, float min_score){
-    string matching = "";
     int ct = 0;
-    for(int i=0; i<genome.length();i++){
+    for(int i=0; i<=genome.length()-sequence1.length();i++){
         float score = similarityScore(genome.substr(i,sequence1.length()),sequence1);
         if(score >= min_score)
             ct++;
@@ -38,6 +56,10 @@ int countMatches(string genome, string sequence1, float min_score){
     return ct;
 }
 
+/*
+* findBestMatch takes two parameters, genome and seq, both strings. and returns a float of the best similarity score found of that sequence within the genome.
+* similar to countMatches, uses a for loop that iterates through the genome string and checks the similarity score with a substring of the genome at that position, which is the length of the  sequence to compare.
+*/
 float findBestMatch(string genome, string seq){
     float bestScore = 0;
     for(int i=0; i<genome.length();i++){
@@ -48,15 +70,22 @@ float findBestMatch(string genome, string seq){
     return bestScore;
 }
 
+/*
+* findBestGenome takes 4 parameters, all strings. the first three parameters are all genomes, and the last parameter is the sequence being matched in this case.
+* it uses findBestMatch on each genome with the sequence.
+* It will return the sequence that is the greatest.
+* If the top two greatest sequences are equal, then it will return 0.
+*/
 int findBestGenome(string genome1, string genome2, string genome3, string seq){
     float g1 = findBestMatch(genome1,seq);
     float g2 = findBestMatch(genome2,seq);
     float g3 = findBestMatch(genome3,seq);
-    if (g3 >= g2 && g3 >= g1)
+
+    if (g3 > g2 && g3 > g1)
         return 3;
-    if(g2 >= g3 && g2 >= g1)
+    if(g2 > g3 && g2 > g1)
         return 2;
-    if(g1 >= g3 && g1 >= g2)
+    if(g1 > g3 && g1 > g2)
         return 1;
 return 0;
 }
